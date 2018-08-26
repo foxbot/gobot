9d188e9b048b3bd37db97fb31b0fc5964f6ffe70
Christopher F <foxbot@protonmail.com>
2018-08-25 23:07:17 -0400

    feature: now playing

---
+++ commands/nowplaying.go
@@ -0,0 +1,30 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/foxbot/gavalink"
+)
+
+const npf = "Currently playing **%s** by **%s** `[%d/%d]`\nSong URL: %s"
+
+func onNowPlaying(c *Context) Response {
+	player, err := c.Lavalink.GetPlayer(c.Event.GuildID)
+	if player == nil {
+		return textResponse("No music is playing on this guild! To play a song use `{{prefix}}play`")
+	}
+	if err != nil {
+		Errors <- err
+		return textResponse("err")
+	}
+
+	t, err := gavalink.DecodeString(player.Track())
+	if err != nil {
+		Errors <- err
+		return textResponse("err")
+	}
+
+	// TODO: prettify durations
+	r := fmt.Sprintf(npf, t.Title, t.Author, t.Position, t.Length, t.URI)
+	return textResponse(r)
+}
