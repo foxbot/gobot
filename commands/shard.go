7f5bf3d4366ebdeaa61e51269eb8d8c9ad4a0a6b
Christopher F <foxbot@protonmail.com>
2018-08-25 22:15:23 -0400

    feature: shard/debug

---
+++ commands/shard.go
@@ -0,0 +1,33 @@
+package commands
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var shard = Command{
+	Aliases: []string{"shard", "debug"},
+	Method:  onShard,
+}
+
+const shardUsage = "Usage: {{prefix}}shard <guild id> <shard count>"
+
+func onShard(ctx *Context) Response {
+	if len(ctx.Args) != 2 {
+		r := fmt.Sprintf("This guild is on shard number `%d` of `%d`.",
+			ctx.Session.ShardID, ctx.Session.ShardCount)
+		return textResponse(r)
+	}
+	id, err := strconv.Atoi(ctx.Args[0])
+	if err != nil {
+		return textResponse(shardUsage)
+	}
+	count, err := strconv.Atoi(ctx.Args[1])
+	if err != nil {
+		return textResponse(shardUsage)
+	}
+
+	shardID := (id >> 22) % count
+	r := fmt.Sprintf("Guild with ID `%d` is on shard `%d`.", id, shardID)
+	return textResponse(r)
+}
